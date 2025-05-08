e3cfe1bd460c73bb3d51c0c61d3c6853d5ac64c8
SonjoyDaGhost <sonjoy.soren.1112@gmail.com>
2025-05-08 23:05:13 +0600

    hi

---
+++ Assignment1/solution.ts
@@ -0,0 +1,103 @@
+function formatString(input: string, toUpper: boolean = true): string {
+    return toUpper ? input.toUpperCase() : input.toLowerCase();
+}
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+    return items.filter(item => item.rating >= 4);
+}
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+    let result: T[] = [];
+    for (const array of arrays) {
+        result = result.concat(array);
+    }
+    return result;
+}
+
+
+class Vehicle {
+    private make: string;
+    private year: number;
+
+    constructor(make: string, year: number) {
+        this.make = make;
+        this.year = year;
+    }
+
+    getInfo(): void {
+        console.log(`Make: ${this.make}, Year: ${this.year}`);
+    }
+}
+
+class Car extends Vehicle {
+    private model: string;
+
+    constructor(make: string, year: number, model: string) {
+        super(make, year);
+        this.model = model;
+    }
+
+    getModel(): void {
+        console.log(`Model: ${this.model}`);
+    }
+}
+
+
+
+
+function processValue(value: string | number): number {
+    if (typeof value === "string") {
+      return value.length;
+    } else {
+      return value * 2;
+    }
+  }
+
+
+  interface Product {
+    name: string;
+    price: number;
+  }
+  
+  function getMostExpensiveProduct(products: Product[]): Product | null {
+    if (products.length === 0) {
+      return null;
+    }
+  
+    let mostExpensive = products[0];
+  
+    for (const product of products) {
+      if (product.price > mostExpensive.price) {
+        mostExpensive = product;
+      }
+    }
+  
+    return mostExpensive;
+  }
+
+  enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+  }
+  
+  function getDayType(day: Day): string {
+    if (day === Day.Saturday || day === Day.Sunday) {
+      return "Weekend";
+    } else {
+      return "Weekday";
+    }
+  }
+
+
+  async function squareAsync(n: number): Promise<number> {
+    if (n < 0) {
+      throw new Error("Negative number not allowed");
+    }
+    return new Promise(resolve => setTimeout(() => resolve(n * n), 1000));
+  }
+  
